cc4a2f767eadb37707d74d56a7cfdbda204ebf90
Patrich <patricholaran01@gmail.com>
2025-10-23 19:38:48 +0800

    test commit

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 echo "<h1>Hello, XAMPP is working!</h1>";
 echo "<p>This is your first PHP file inside VS Code.</p>";
-
-
+echo "Kylelord The GOAT!";
+echo "There i was, an empty piece of a shell.";
 
 ?>
 <p>Test change by [your name]</p>
