49832bb36d248a1fbccafc14419313fd9be5b34c
Francis Kyle Lumayaga <lumayagafranciskyle@gmail.com>
2025-10-23 12:03:33 +0800

    added sht

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Booking-System-for-Medical-Clinics
 Project 1 From Lumayaga, Libando, Olaran
+
+
+Guys mao ning collaborative work
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+echo "<h1>Hello, XAMPP is working!</h1>";
+echo "<p>This is your first PHP file inside VS Code.</p>";
+
+
+
+?>
+<p>Test change by [your name]</p>
