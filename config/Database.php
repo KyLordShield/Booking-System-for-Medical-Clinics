39f316f44a2d97eb2a5f879c4ebd79999b5246d1
Francis Kyle Lumayaga <lumayagafranciskyle@gmail.com>
2025-10-23 21:54:07 +0800

    added database connection

---
+++ config/Database.php
@@ -0,0 +1,35 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+class Database {
+    // Database credentials
+    private $host = "localhost";
+    private $dbname = "medical_booking";
+    private $username = "root";
+    private $password = "";
+    private $conn;
+
+    // Connect to the database
+    public function connect() {
+        if ($this->conn === null) {
+            try {
+                // Create a PDO connection
+                $this->conn = new PDO(
+                    "mysql:host={$this->host};dbname={$this->dbname};charset=utf8mb4",
+                    $this->username,
+                    $this->password
+                );
+
+                // Set PDO error mode to exception
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            } catch (PDOException $e) {
+                echo "Connection failed: " . $e->getMessage();
+            }
+        }
+
+        return $this->conn;
+    }
+}
+?>
